05521548be79eda722b887a97c5316d6795301cc
Mlodkoo <63650085+Mlodko@users.noreply.github.com>
2024-04-08 21:23:04 +0200

    Saving calendar, board and card state to json

--- calendar-app/src/main/java/client/backend/models/KanbanBoard.java
+++ calendar-app/src/main/java/client/backend/models/KanbanBoard.java
@@ -1,6 +1,8 @@
 package client.backend.models;
 
+import client.backend.serialization.CardIdSerializer;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 
 import java.text.ParseException;
@@ -8,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class KanbanBoard implements Savable<KanbanBoard> {
@@ -19,15 +22,15 @@ public class KanbanBoard implements Savable<KanbanBoard> {
     @Expose String title; // Not md! >:(
     @Expose Date createTime;
     @Expose Date lastModifiedTime;
-    @Expose Calendar calendar;
+    Calendar calendar;
+    @Expose private String calendarId;
 
     // This is a structure that contains all "columns"/sub-lists of a kanban board
     // Title -> ArrayList<Item>
-    @Expose HashMap<String, ArrayList<KanbanInsertable>> itemsLists;
+    HashMap<String, ArrayList<KanbanInsertable>> itemsLists;
+    @Expose private HashMap<String, List<String>> itemIds;
 
-    public KanbanBoard(String id) {
-        this.id = id;
-    }
+    public KanbanBoard(String id) {this.id = id;}
 
     public KanbanBoard(String id, String title, Date createTime, Date lastModifiedTime, Calendar calendar,
                        HashMap<String, ArrayList<KanbanInsertable>> itemsLists) {
@@ -36,7 +39,12 @@ public class KanbanBoard implements Savable<KanbanBoard> {
         this.createTime = createTime;
         this.lastModifiedTime = lastModifiedTime;
         this.calendar = calendar;
+        this.calendarId = calendar.getID();
         this.itemsLists = itemsLists;
+        this.itemIds = new HashMap<>();
+        itemsLists.forEach((columnTitle, items) -> {
+            itemIds.put(columnTitle, items.stream().map(KanbanInsertable::getId).toList());
+        });
     }
 
     public String getId() {
@@ -85,6 +93,7 @@ public class KanbanBoard implements Savable<KanbanBoard> {
 
     public void setCalendar(Calendar calendar) {
         this.calendar = calendar;
+        this.calendarId = calendar.getID();
     }
 
     public HashMap<String, ArrayList<KanbanInsertable>> getItemsLists() {
@@ -93,6 +102,9 @@ public class KanbanBoard implements Savable<KanbanBoard> {
 
     public void setItemsLists(HashMap<String, ArrayList<KanbanInsertable>> itemsLists) {
         this.itemsLists = itemsLists;
+        itemsLists.forEach((columnTitle, items) -> {
+            this.itemIds.put(columnTitle, items.stream().map(KanbanInsertable::getId).toList());
+        });
     }
 
     public void addToItemsList(String columnTitle, KanbanInsertable item) throws java.util.NoSuchElementException{
@@ -101,6 +113,7 @@ public class KanbanBoard implements Savable<KanbanBoard> {
                     "\nTry calling addNewItemColumn()" );
         }
         itemsLists.get(columnTitle).add(item);
+        itemIds.get(columnTitle).add(item.getId());
     }
 
     public void addToItemsList(String columnTitle, ArrayList<KanbanInsertable> items) throws java.util.NoSuchElementException{
@@ -109,6 +122,7 @@ public class KanbanBoard implements Savable<KanbanBoard> {
                     "\nTry calling addNewItemColumn()" );
         }
         itemsLists.get(columnTitle).addAll(items);
+        itemIds.get(columnTitle).addAll(items.stream().map(KanbanInsertable::getId).toList());
     }
 
     public void addNewItemColumn(String columnTitle) throws IllegalArgumentException{
@@ -117,6 +131,7 @@ public class KanbanBoard implements Savable<KanbanBoard> {
                     "\nTry calling addToItemsList()");
         }
         itemsLists.put(columnTitle, new ArrayList<KanbanInsertable>());
+        itemIds.put(columnTitle, new ArrayList<>());
     }
 
     public void addNewItemColumn(String columnTitle, ArrayList<KanbanInsertable> items) {
@@ -125,6 +140,7 @@ public class KanbanBoard implements Savable<KanbanBoard> {
                     "\nTry calling addToItemsList()");
         }
         itemsLists.put(columnTitle, items);
+        itemIds.put(columnTitle, items.stream().map(KanbanInsertable::getId).toList());
     }
 
     @Override
@@ -134,6 +150,6 @@ public class KanbanBoard implements Savable<KanbanBoard> {
 
     @Override
     public String saveToString() {
-        return new Gson().toJson(this);
+        return new GsonBuilder().registerTypeAdapter(Card.class, new CardIdSerializer()).create().toJson(this);
     }
 }

2e76bc27e069f20e7db31308a843c75ab359afde
Mlodko <mlodkowskip@gmail.com>
2024-04-16 07:31:01 +0200

    Backend skończony(????)

---
+++ calendar-app/src/main/java/client/frontend/MarkdownParser.java
@@ -0,0 +1,15 @@
+package client.frontend;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+abstract public class MarkdownParser {
+
+    final static Parser parser = Parser.builder().build();
+    final static HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+    public static String parseToHtml(String markdown) {
+        Node document = parser.parse(markdown);
+        return renderer.render(document);
+    }
+}

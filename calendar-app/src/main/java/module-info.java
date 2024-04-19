354d1c249c44f02b97cc847e74ec5d83726444a9
Mlodkoo <63650085+Mlodko@users.noreply.github.com>
2024-04-20 01:26:58 +0200

    Frontend uwu

--- calendar-app/src/main/java/module-info.java
+++ calendar-app/src/main/java/module-info.java
@@ -4,9 +4,11 @@ module client.backend {
     requires com.google.gson;
     requires org.commonmark;
     requires javafx.web;
+    requires org.apache.commons.codec;
 
 
     opens com.example.calendarapp to javafx.fxml;
+    exports client.frontend;
     exports com.example.calendarapp;
     opens client.backend.models to com.google.gson;
     opens client.backend to com.google.gson;

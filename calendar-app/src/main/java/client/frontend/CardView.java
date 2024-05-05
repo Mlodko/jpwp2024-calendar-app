8caf546a06247eabfcbef8cc3d275cb7d28131c3
Stanisław <sniemczewski@onet.pl>
2024-05-05 16:50:08 +0200

    card window almost finished, still needs backend support

--- calendar-app/src/main/java/client/frontend/CardView.java
+++ calendar-app/src/main/java/client/frontend/CardView.java
@@ -8,60 +8,69 @@ import javafx.geometry.Pos;
 import javafx.print.Printer;
 import javafx.print.PrinterJob;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.web.HTMLEditor;
 import javafx.scene.web.WebView;
-import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
+import java.util.Stack;
+
 public class CardView {
 
-    Parser parser = Parser.builder().build();
-    HtmlRenderer renderer = HtmlRenderer.builder().build();
-    WebView webView = new WebView();
-    HTMLEditor htmlEditor = new HTMLEditor();
+    private Parser parser = Parser.builder().build();
+    private HtmlRenderer renderer = HtmlRenderer.builder().build();
+    private WebView webView = new WebView();
+    private String cardCache = new String();
 
     public Scene createCardView(Card card) {
         StackPane mainStack = new StackPane();
         VBox mainVBox = new VBox();
 
         HBox top = new HBox();
-        //top.setAlignment(Pos.TOP_CENTER);
+        top.setAlignment(Pos.TOP_CENTER);
         top.setSpacing(10);
         top.setPadding(new Insets(5,5,5,5));
 
-        AnchorPane forTitle = new AnchorPane();
+        AnchorPane forTitleTexts = new AnchorPane();
+        HBox titleTexts = new HBox();
         Label title = new Label(card.getTitle());
         title.setAlignment(Pos.CENTER);
+        Label mode = new Label("Preview Mode");
+        mode.setAlignment(Pos.CENTER);
 
-        AnchorPane forButtons = new AnchorPane();
+        HBox forButtons = new HBox();
+        forButtons.setAlignment(Pos.CENTER_RIGHT);
+        forButtons.setSpacing(10);
         Button edit = new Button("Edit card");
         Button read = new Button("Preview card");
 
         StackPane textField = this.setupReadView(card);
 
         edit.setOnAction(event -> {
-            // TODO enter edit mode
+            mode.setText("Edit Mode");
+            mainVBox.getChildren().set(2, this.setupEditView(card));
         });
 
         read.setOnAction(event -> {
-            textField.getChildren().set(0, this.setupReadView(card));
+            mode.setText("Preview Mode");
+            mainVBox.getChildren().set(2, this.setupReadView(card));
         });
 
-        HBox.setHgrow(forButtons, Priority.ALWAYS);
-        forTitle.getChildren().add(title);
+        Separator vsep = new Separator(Orientation.VERTICAL);
+        titleTexts.setSpacing(5);
+        titleTexts.getChildren().addAll(title, vsep, mode);
+
+        HBox.setHgrow(forTitleTexts, Priority.ALWAYS);
+        forTitleTexts.getChildren().add(titleTexts);
         forButtons.getChildren().addAll(edit, read);
-        top.getChildren().addAll(forTitle, forButtons);
+        top.getChildren().addAll(forTitleTexts, forButtons);
 
         Separator sep = new Separator();
 
         mainVBox.setSpacing(5);
         mainVBox.setPadding(new Insets(5,5,5,5));
+        VBox.setVgrow(textField, Priority.ALWAYS);
         mainVBox.getChildren().addAll(top, sep, textField);
         mainStack.setPadding(new Insets(3,3,3,3));
         mainStack.getChildren().add(mainVBox);
@@ -70,24 +79,47 @@ public class CardView {
     }
 
     private StackPane setupReadView(Card card) {
-        StackPane mainStack = new StackPane();
-        VBox mainVBox = new VBox();
-        mainVBox.setSpacing(5);
-
         String htmlCard = renderer.render(parser.parse(card.getDescription()));
         webView.getEngine().loadContent(htmlCard);
         StackPane mainField = new StackPane();
         mainField.getChildren().add(webView);
+        mainField.setPadding(new Insets(3,3,3,3));
 
-        mainVBox.getChildren().addAll(mainField);
-        mainStack.setPadding(new Insets(3,3,3,3));
-        mainStack.getChildren().add(mainVBox);
-
-        return mainStack;
+        return mainField;
     }
-/*
+
     private StackPane setupEditView(Card card) {
+        String cardContent = card.getDescription();
+        TextArea textArea = new TextArea();
+        textArea.setText(cardContent);
+        textArea.setWrapText(true);
+        textArea.setEditable(true);
 
+        // Bind the TextArea's text property to itself to preserve new lines
+        textArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.equals(oldValue)) {
+                textArea.setText(newValue);
+            }
+        });
+
+        Button save = new Button("Save card");
+        save.setOnAction(event -> {
+            card.setDescription(textArea.getText().replaceAll("\n", System.getProperty("line.separator")));
+            // TODO save to files and server!
+        });
+
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER_RIGHT);
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(3,3,3, 3));
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+        vbox.getChildren().addAll(textArea, save);
+
+        StackPane mainField = new StackPane();
+        mainField.setAlignment(Pos.CENTER);
+        mainField.getChildren().add(vbox);
+
+        return mainField;
     }
-*/
+
 }
